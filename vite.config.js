a5f90f3c9b89b021f6338c4f99bfa11cee85b945
@NJ <jangalen71@gmail.com>
2025-03-27 21:15:12 +0530

    updated commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/Recipe_Finder2/',  // Set base to repo name
+});
+
+
+
+
